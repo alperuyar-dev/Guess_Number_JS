88ed29dc86674800a9602b4da2c87c6c40d1f598
Alper Uyar <alperuyar@192.168.1.56>
2022-08-24 22:38:24 +0300

    updated app.js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const guessButton = document.getElementById("guess-button");
 const tryButton = document.getElementById("try-button");
 const result_Text = document.getElementById("result");
+const easter = document.getElementById("easter");
 result_Text.style.display = "none";
+easter.style.display = "none";
 tryButton.style.display = "none";
 let life = 2;
+
 const randomNum = Math.floor(Math.random() * 11);
 function guessNumber() {
   const inputNumber = document.getElementById("input-number").value;
@@ -16,17 +19,25 @@ function guessNumber() {
     guessButton.style.display = "none";
     tryButton.style.display = "block";
     tryButton.innerHTML = "Feel Lucky ?";
+  } else if (inputNumber == 31 && randomNum != inputNumber) {
+    life--;
+    result_Text.style.display = "none";
+    easter.style.display = "block";
+    console.log("wwwoooww");
   } else if (randomNum !== inputNumber) {
     life--;
     result_Text.style.display = "block";
+    easter.style.display = "none";
     result_Text.innerHTML = "Nope";
   }
+
   if (life === -1) {
     guessButton.style.display = "none";
     tryButton.style.display = "block";
     result_Text.style.display = "block";
     result_Text.innerHTML = "Good Try";
   }
+
   console.log(randomNum);
   console.log(inputNumber);
   console.log("life: " + life);
